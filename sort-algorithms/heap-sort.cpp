04e4ba07c65259de8d450a0cb2c9e8794b3f35bb
RafeedMohammad11 <rafeed.cse@gmail.com>
2026-04-23 16:46:35 +0600

    feat: add B+ Tree implementation with documentation and implement Heap Sort algorithm

--- sort-algorithms/heap-sort.cpp
+++ sort-algorithms/heap-sort.cpp
@@ -45,9 +45,16 @@ int main()
 {
     int arr[] = {12, 11, 13, 5, 6, 7};
     int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Unsorted Array: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 
     heapSort(arr, n);
 
+    cout << "Sorted Array:" << endl;
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
