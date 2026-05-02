8161f22507809561c1a54dde699f484237d6c360
RafeedMohammad11 <rafeed.cse@gmail.com>
2026-05-02 15:25:04 +0600

    Implement Apriori algorithm with transaction reading and support counting functions

---
+++ apirori-algorithm/apriori.cpp
@@ -0,0 +1,119 @@
+/*
+Basic terminologies:
+Support: The frequency of an itemset in the dataset
+Confidence: The probability that an item appears in a transaction given the presence of another item
+Lift: The strength of the association, indicating whether items are bought together more often than expected
+
+
+Apriori Algorithm Steps
+ 1. Generate candidate item sets - Identifying individual items and counting their occurrences to determine frequent items
+ 2. Prune item sets - items that appear less than the minimum support threshold are removed
+ 3. Form frequent item sets - generates larger itemsets by combining frequent smaller itemsets, iterating until no more frequent itemsets can be found
+ 4. Derive association rules - extracts rules based on confidence and lift values to determine relationships
+*/
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <sstream>
+#include <map>
+using namespace std;
+
+// Defined Interfaces
+vector<set<string>> readTransactions(int n);
+void printTransactions(const vector<set<string>> &transactions);
+
+map<string, int> countSingleItemSupport(const vector<set<string>> &transactions);
+void printSupportCount(const map<string, int> &supportCount);
+
+int main()
+{
+    int n; // no. of transactions
+    cout << "Enter number of transactions:";
+    cin >> n;
+    cin.ignore(); // clear newline
+
+    vector<set<string>> transactions = readTransactions(n);
+
+    printTransactions(transactions);
+
+    map<string, int> supportCount = countSingleItemSupport(transactions);
+    printSupportCount(supportCount);
+
+    int minSupport;
+    cout << "Enter minimum support: ";
+    cin >> minSupport;
+
+    
+
+    return 0;
+}
+
+// Function to read transactions
+vector<set<string>> readTransactions(int n)
+{
+    vector<set<string>> transactions;
+    cout << "Enter each transactions (space separated items): " << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        string item;
+        set<string> trans;
+
+        while (ss >> item)
+        {
+            trans.insert(item);
+        }
+
+        transactions.push_back(trans);
+    }
+
+    return transactions;
+}
+
+// Function to print transactions
+void printTransactions(const vector<set<string>> &transactions)
+{
+    cout << "Stored transactions:" << endl;
+
+    for (int i = 0; i < transactions.size(); i++)
+    {
+        cout << "T" << i + 1 << ": { ";
+        for (auto item : transactions[i])
+        {
+            cout << item << " ";
+        }
+        cout << "}";
+
+        cout << endl;
+    }
+}
+
+map<string, int> countSingleItemSupport(const vector<set<string>> &transactions)
+{
+    map<string, int> supportCount;
+
+    for (auto &trans : transactions)
+    {
+        for (auto &item : trans)
+        {
+            supportCount[item]++;
+        }
+    }
+
+    return supportCount;
+}
+
+// Function to print supoort counts
+void printSupportCount(const map<string, int> &supportCount)
+{
+    cout << "\nSupport count of 1-itemsets:" << endl;
+
+    for (auto &entry : supportCount)
+    {
+        cout << entry.first << " -> " << entry.second << endl;
+    }
+}
