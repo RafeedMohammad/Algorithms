a7be21d1f6930d313fb8831812ba92f581662edd
RafeedMohammad <rafeedmohammad1107@gmail.com>
2026-05-18 01:02:02 +0600

    hashing refined

--- data-structures/hashing-chaining.cpp
+++ data-structures/hashing-chaining.cpp
@@ -12,60 +12,73 @@
 #include <iostream>
 using namespace std;
 
+enum HashMethod
+{
+    MID_SQUARE,
+    FOLDING
+};
+
 struct Node
 {
     int key;
     Node *next;
+    Node(int k) : key(k), next(nullptr) {}
 };
 
 class HashTable
 {
 private:
     int size;
-    Node **table;
-
-public:
-    HashTable(int s)
-    {
-        size = s;
-        table = new Node *[size];
-
-        for (int i = 0; i < size; i++)
-        {
-            table[i] = NULL;
-        }
-    }
+    int foldSize;
+    HashMethod method;
+    Node **table; // table - pointer to first element of array, each array elements - Node*
 
     // --------Mid Square Method-----------
     int midSquareHash(int key)
     {
-        long long sq = 1LL * key * key;
+        long long sq = 1LL * key * key; // 1LL - to avoid overflow
         string s = to_string(sq);
-
         int len = s.length();
-        int mid = len / 2;
+
+        int start = (len - 2) / 2; // mid position
 
         string midDigits;
-
         if (len >= 2)
         {
-            if (mid == 0)
-            {
-                midDigits = 5;
-            }
-
-            else
-            {
-                midDigits = s.substr(mid - 1, 2);
-            }
+            midDigits = s.substr(start, 2);
         }
-
         else
         {
-            midDigits = 5;
+            midDigits = s;
         }
 
-        int midVal = stoi(midDigits);
-        return midVal % size;
+        return stoi(midDigits) % size; // stoi converts string into integer
+    }
+
+    //-----------Folding Method--------------
+    int foldingHash(int key)
+    {
+        string s = to_string(key);
+        int sum = 0, i = 0;
+
+        while (i < (int)s.length())
+        {
+            sum += stoi(s.substr(i, foldSize));
+            i += foldSize;
+        }
+    }
+
+public:
+    HashTable(int s, HashMethod m, int fs = 2)
+    {
+        size = s;
+        method = m;
+        foldSize = fs;
+        table = new Node *[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            table[i] = nullptr;
+        }
     }
 };
