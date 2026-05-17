cd5cc9e460f08aca3e44a3985ccf221d328b59e9
RafeedMohammad <rafeedmohammad1107@gmail.com>
2026-05-18 02:49:42 +0600

    hashcode completed

--- data-structures/hashing-chaining.cpp
+++ data-structures/hashing-chaining.cpp
@@ -66,6 +66,16 @@ private:
             sum += stoi(s.substr(i, foldSize));
             i += foldSize;
         }
+
+        return sum % size;
+    }
+
+    //------------Method selection---------------
+    int hash(int key)
+    {
+        return (method == MID_SQUARE)
+                   ? midSquareHash(key)
+                   : foldingHash(key);
     }
 
 public:
@@ -81,4 +91,95 @@ public:
             table[i] = nullptr;
         }
     }
+
+    ~HashTable()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            Node *curr = table[i];
+            while (curr)
+            {
+                Node *tmp = curr;
+                curr = curr->next;
+                delete tmp;
+            }
+        }
+
+        delete[] table;
+    }
+
+    void insert(int key)
+    {
+        int idx = hash(key);
+        Node *newNode = new Node(key);
+        newNode->next = table[idx];
+        table[idx] = newNode;
+        cout << "inserted " << key << " -> index " << idx << endl;
+    }
+
+    bool search(int key)
+    {
+        int idx = hash(key);
+        Node *curr = table[idx];
+        while (curr)
+        {
+            if (curr->key == key)
+            {
+                return true;
+            }
+            curr = curr->next;
+        }
+        return false;
+    }
+
+    void display()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << " [" << i << "] -> ";
+            Node *curr = table[i];
+            while (curr)
+            {
+                cout << curr->key << "->";
+                curr = curr->next;
+            }
+
+            cout << "NULL" << endl;
+        }
+    }
+
+    void searchAndPrint(int key)
+    {
+        cout << "Search " << key << ": " << (search(key) ? "found" : "not found") << endl;
+    }
 };
+
+int main()
+{
+    int keys[] = {79, 25, 36, 123, 456, 79};
+    cout << "=== Mid-Square Hashing (size=10) ===\n";
+    HashTable ms(10, MID_SQUARE);
+    for (int k : keys)
+        ms.insert(k);
+    cout << "\nHash Table:\n";
+    ms.display();
+
+    // cout << "\nSearch 36: " << (ms.search(36) ? "found" : "not found") << endl;
+    // cout << "Search 123: " << (ms.search(123) ? "found" : "not found") << endl;
+
+    ms.searchAndPrint(36);
+    ms.searchAndPrint(125);
+    cout << endl;
+
+    cout << "=== Folding Method (size=10, foldSize=2) ===\n";
+    HashTable f1(10, FOLDING, 2);
+    for (int k : keys)
+    {
+        f1.insert(k);
+    }
+
+    cout << "\nHash Table: " << endl;
+    f1.display();
+
+    return 0;
+}
