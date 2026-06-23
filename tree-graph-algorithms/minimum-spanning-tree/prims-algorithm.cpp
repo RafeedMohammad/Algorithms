af9c8748b49a9d7716bd9f6f0d3c98d1c9877d75
rafeedmohammad11 <rafeed.cse@gmail.com>
2026-06-23 11:53:40 +0600

    feat: implement Prims algorithm for MST

---
+++ tree-graph-algorithms/minimum-spanning-tree/prims-algorithm.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+using namespace std;
+
+using PII = pair<int, int>;
+
+int V = 5;
+vector<vector<pair<int, int>>> adj(V);
+
+vector<int> minEdge(V, INT_MAX);
+vector<int> parent(V, -1);
+vector<bool> inMST(V, false); // whether vertex is already in tree or not
+
+priority_queue<
+    PII,
+    vector<PII>,
+    greater<PII>>
+    pq;
+
+void addEdge(int u, int v, int w)
+{
+    adj[u].push_back({v, w});
+    adj[v].push_back({u, w});
+}
+
+// void printPQ()
+// {
+//     priority_queue<
+//         PII,
+//         vector<PII>,
+//         greater<PII>>
+//         temp = pq;
+
+//     cout << "Priority Queue:\n";
+
+//     while (!temp.empty())
+//     {
+//         cout << "("
+//              << temp.top().first
+//              << ", "
+//              << temp.top().second
+//              << ")\n";
+
+//         temp.pop();
+//     }
+
+//     cout << endl;
+// }
+
+void prim(int src)
+{
+    minEdge[src] = 0;
+    pq.push({0, src});
+
+    while (!pq.empty())
+    {
+        int u = pq.top().second;
+        pq.pop();
+
+        if (inMST[u])
+        {
+            continue;
+        }
+
+        inMST[u] = true;
+
+        for (auto [v, w] : adj[u])
+        {
+            if (!inMST[v] && w < minEdge[v])
+            {
+                parent[v] = u;
+                minEdge[v] = w;
+                pq.push({w, v});
+            }
+        }
+    }
+}
+
+void printMST()
+{
+    int totalWeight = 0;
+    cout << "MST Edges: " << endl;
+    for (int v = 1; v < V; v++)
+    {
+        cout << parent[v] << " - " << v
+             << " weight: " << minEdge[v] << endl;
+
+        totalWeight += minEdge[v];
+    }
+
+    cout << "total weight: " << totalWeight << endl;
+}
+
+int main()
+{
+    addEdge(0, 1, 2);
+    addEdge(0, 2, 6);
+    addEdge(0, 3, 1);
+    addEdge(1, 4, 5);
+    addEdge(2, 4, 1);
+    addEdge(3, 4, 4);
+
+    prim(0);
+
+    printMST();
+
+    return 0;
+}
